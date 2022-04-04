e8946df1b1ccf9823cef7905ec18b7ef07732a7c
nkgrr1004 <nikhilrrgupta.com>
2022-04-04 12:53:11 +0530

    commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const about = () => {
+  return (
+    <h1>
+      <Meta title="About" />
+      <h1>About</h1>
+    </h1>
+  );
+};
+
+export default about;
